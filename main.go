8727c950c287ff790b0dc4610c71ab706c8b9c03
Pratam-Kalligudda <kalliguddapratam642002@gmail.com>
2025-10-05 22:48:05 +0530

    initial setup added docker compose for the setup

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+)
 func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Get("/health",func (ctx fiber.Ctx)error{
+		return ctx.Status(fiber.StatusOK).JSON(map[string]string{"message":"healthy"})
+	})
+	app.Listen(":8080")
 	fmt.Println("hello world")
 }
